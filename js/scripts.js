e482d854ea0882334dfc5b0036f043b286cdb024
Robert Thomas <rtd2.v1@gmail.com>
2015-01-01 20:04:59 -0500

    function definitions before their use, other linting

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,14 @@ var hist = [];
 var histindex = 0;
 var count = 0;
 
-commands.addEventListener("keyup", checkCommand, false);
+function addToHistory(command) {
+    
+    if (hist.slice(-1) !== command) {
+        
+        hist.push(command);
+        
+    }
+}
 
 function checkCommand(e) {
 
@@ -75,10 +82,10 @@ function checkCommand(e) {
             histindex++;
             commands.size = hist[histindex].length + 1;
             commands.value = hist[histindex];
-            
             count--;
 
         } else {
+            
             commands.value = "";
             commands.size = 1;
             count = 0;
@@ -91,25 +98,29 @@ function checkCommand(e) {
         
         if (commandArgs.length === 2) {
 
-            switch(commandArgs[0]){
+            switch (commandArgs[0]) {
 
                 case "mkdir": // if folder doesn't exist, create it and add to fs
 
-                        var folderName = commandArgs.slice(1).join(" ");
+                    var folderName = commandArgs.slice(1).join(" ");
 
-                        if (pwd[1].hasOwnProperty(folderName)) {
-                            history.innerHTML += webtermHTML;
-                            history.innerHTML += "<p>mkdir: cannot create directory '" + folderName + "': File exists</p>";
-                        } else {
-                            pwd[1][folderName] = {files: []};
-                            history.innerHTML += webtermHTML;
-                            history.innerHTML += "<p>Folder called " + folderName + " successfully created.</p>";
-                        }
+                    if (pwd[1].hasOwnProperty(folderName)) {
+                        
+                        history.innerHTML += webtermHTML;
+                        history.innerHTML += "<p>mkdir: cannot create directory '" + folderName + "': File exists</p>";
+                        
+                    } else {
+                        
+                        pwd[1][folderName] = {files: []};
+                        history.innerHTML += webtermHTML;
+                        history.innerHTML += "<p>Folder called " + folderName + " successfully created.</p>";
+                        
+                    }
 
-                        addToHistory(command);
-                        count = 0;
+                    addToHistory(command);
+                    count = 0;
 
-                        break;
+                break;
                     
                 case "touch": // if file doesn't exist, create it and add to documents folder
 
@@ -117,36 +128,38 @@ function checkCommand(e) {
 
                     var files = pwd[1].files;
                                         
-                    if (files.indexOf(fileName) != -1) {
+                    if (files.indexOf(fileName) !== -1) {
+                        
                         history.innerHTML += webtermHTML;
                         history.innerHTML += "<p>touch: cannot create file '" + fileName + "': File exists</p>";
+                        
                     } else {
+                        
                         files.push(fileName);
                         history.innerHTML += webtermHTML;
                         history.innerHTML += "<p>File called " + fileName + " successfully created.</p>";
+                        
                     }
 
                     addToHistory(command);
                     count = 0;
 
-                    break;
+                break;
                     
                 case "cd": // change directory
 
                     var directory = commandArgs.slice(1).join(" ");
                     
-                    
-                    
                     addToHistory(command);
                     count = 0;
 
-                    break;
+                break;
 
             }
             
         } else if (commandArgs.length > 2) {
 
-            switch(commandArgs[0] + " " + commandArgs[1]){
+            switch (commandArgs[0] + " " + commandArgs[1]) {
 
                 case "youtube -s": // search youtube with argument passed in
 
@@ -154,14 +167,18 @@ function checkCommand(e) {
                     var term = "";
 
                     for (var i = 2; i < commandArgs.length; i++) {
+                        
                         term += commandArgs[i] + "%20";
+                        
                     }
 
                     term = term.slice(0,-3);
 
                     if (term !== "") {
+                        
                         var url = base + term;
                         window.open(url, '_blank');
+                        
                     }
 
                     history.innerHTML += webtermHTML;
@@ -169,109 +186,114 @@ function checkCommand(e) {
                     addToHistory(command);
                     count = 0;
 
-                    break;
+                break;
                     
             } //switch
 
-        }   else {
+        } else {
 
-                switch(command){
+            switch (command) {
 
-                    case "help": // show list of commands
+                case "help": // show list of commands
 
-                        history.innerHTML += webtermHTML;
-                        var displayCommands = "";
-                        // display help
-                        for (var i = 0; i < helpList.length; i++) {
-                            displayCommands += "<p>" + helpList[i] + "</p>";
-                        }
-
-                        history.innerHTML += displayCommands;
-
-                        addToHistory(command);
-                        count = 0;
-
-                        break;
-
-                    case "cd /home/rt/desktop": // fake cd functionality
-
-                        history.innerHTML += webtermHTML;
-
-                        pwd = ["/home/rt/desktop", fs.home.rt.desktop, "/home/rt/desktop"];
-                        document.getElementById("block").innerHTML = "WebTerm:" + pwd[0] + " rt$ ";
-
-                        addToHistory(command);
-                        count = 0;
-
-                        break;
-
-                    case "pwd": // show present working directory
-
-                        history.innerHTML += webtermHTML;
-
-                        history.innerHTML += "<p>" + pwd[2] + "</p>";
-
-                        addToHistory(command);
-                        count = 0;
-
-                        break;
-
-                    case "ll": // show all files in fs/home/rt
-
-                        var list = "";
-
-                        // loop through objects in fs to show files
-                        for (var key in pwd[1]) {
-                           var obj = pwd[1][key];
-                           for (var prop in obj) {
-                              // important check that this is objects own property 
-                              // not from prototype prop inherited
-                              if(obj.hasOwnProperty(prop) && obj[prop].length > 0){
-                                list += "<p class='file'>" + key + " = " + obj[prop] + "</p>";
-                              } else {list += "<p class='file'>" + key + " = empty</p>";}
-                           }
-                        }
-
-                        history.innerHTML += webtermHTML;
-
-                        history.innerHTML += "<p>" + list + "</p>";
-
-                        addToHistory(command);
-                        count = 0;
-
-                        break;
-
-                    case "clear": //clear page
-
-                        history.innerHTML = "";
-
-                        addToHistory(command);
-                        count = 0;
-
-                        break;
-
-                    case "youtube": // open youtube in new tab
-
-                        history.innerHTML += webtermHTML;
-
-                        window.open('http://www.youtube.com','_blank');
-
-                        addToHistory(command);
-                        count = 0;
-
-                        break;
-                        
-                    case "cd": // change directory
-
-                        pwd = ["~", fs.home.rt, "/home/rt"];
+                    history.innerHTML += webtermHTML;
+                    var displayCommands = "";
                     
-                        addToHistory(command);
-                        count = 0;
+                    for (var i = 0; i < helpList.length; i++) { // display help
+                        
+                        displayCommands += "<p>" + helpList[i] + "</p>";
+                        
+                    }
 
-                    break;
+                    history.innerHTML += displayCommands;
 
-                    default:
-                        history.innerHTML = "<p>No such command exists. Type 'help' for a list of commands.</p>";
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                case "cd /home/rt/desktop": // fake cd functionality
+
+                    history.innerHTML += webtermHTML;
+
+                    pwd = ["/home/rt/desktop", fs.home.rt.desktop, "/home/rt/desktop"];
+                    document.getElementById("block").innerHTML = "WebTerm:" + pwd[0] + " rt$ ";
+
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                case "pwd": // show present working directory
+
+                    history.innerHTML += webtermHTML;
+
+                    history.innerHTML += "<p>" + pwd[2] + "</p>";
+
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                case "ll": // show all files in fs/home/rt
+
+                    var list = "";
+
+                    
+                    for (var key in pwd[1]) { // loop through objects in fs to show files
+                        var obj = pwd[1][key];
+                        for (var prop in obj) { // important check that this is objects own property not from prototype prop inherited
+                            if (obj.hasOwnProperty(prop) && obj[prop].length > 0) {
+                                
+                                list += "<p class='file'>" + key + " = " + obj[prop] + "</p>";
+                                
+                            } else {
+                                
+                                list += "<p class='file'>" + key + " = empty</p>";
+                            
+                            }
+                        }
+                    }
+
+                    history.innerHTML += webtermHTML;
+                    history.innerHTML += "<p>" + list + "</p>";
+
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                case "clear": //clear page
+
+                    history.innerHTML = "";
+
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                case "youtube": // open youtube in new tab
+
+                    history.innerHTML += webtermHTML;
+
+                    window.open('http://www.youtube.com','_blank');
+
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                case "cd": // change directory
+
+                    pwd = ["~", fs.home.rt, "/home/rt"];
+
+                    addToHistory(command);
+                    count = 0;
+
+                break;
+
+                default:
+                    history.innerHTML = "<p>No such command exists. Type 'help' for a list of commands.</p>";
             }
         }
 
@@ -281,9 +303,4 @@ function checkCommand(e) {
 
 } // checkCommand
 
-
-function addToHistory(command) {
-    if(hist.slice(-1) != command){
-        hist.push(command);
-    }
-}
+commands.addEventListener("keyup", checkCommand, false);
