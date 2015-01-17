c49fc92b0befeb64bcea144add6972aa6b9a965b
Seth Shober <seth.shober@gmail.com>
2015-01-16 21:31:28 -0800

    create dev branch and add curly braces to index function to make safer

--- js/scripts.js
+++ js/scripts.js
@@ -749,14 +749,13 @@ function getDirectory(directory) {
 }
 
 function index(obj,is, value) {
-    if (typeof is == 'string')
-        return index(obj, is.split('.'), value);
-    else if (is.length == 1 && value !== undefined)
-        return obj[is[0]] = value;
-    else if (is.length == 0)
-        return obj;
-    else
-        return index(obj[is[0]],is.slice(1), value);
+    if (typeof is == 'string') { return index(obj, is.split('.'), value); }
+    
+    else if (is.length == 1 && value !== undefined) { return obj[is[0]] = value; }
+
+    else if (is.length == 0) { return obj; }
+
+    else { return index(obj[is[0]],is.slice(1), value); }
 }
 
 function replaceAll(find, replace, str) {
